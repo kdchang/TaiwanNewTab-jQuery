af70b2a9d0c16ed8659e95bcdb966ef266953fa6
kdchang <kdchang.cc@gmail.com>
2016-01-23 20:39:17 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TaiwanNewTab(jQuery)
+分享台灣的美好風景
+
+TaiwanNewTab(台灣風景分頁) 讓使用者可以在開新分頁時欣賞台灣各地風景名勝，在工作之餘可以放鬆心情，欣賞美景。
+
+本 Google Chrome Extension 為教學使用，圖片取自 Flickr，並附上原圖連結，若有侵權煩請來信告知，謝謝您。
---
+++ scripts/background.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+    function loadPhoto() {
+        //var url = 'https://raw.githubusercontent.com/kdchang/TaiwanNewTab/master/photos.json';
+        var url = '../photos.json';
+
+        // get images
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: url,
+            success: function(response) {
+                var imgSource = [];
+                var imgLink = [];
+                var imgName = [];
+                var imgAuthor = [];
+                var rand = 0;
+                var showImgSource = '';
+                var showImgLink = '';
+                var showImgName = '';
+                var showImgAuthor = '';
+
+                for (var key in response) {
+                    imgSource.push(response[key].path);
+                    imgLink.push(response[key].link);
+                    imgName.push(response[key].name);
+                    imgAuthor.push(response[key].authorName);
+                }
+
+                rand = Math.floor(Math.floor(Math.random() * imgSource.length));
+
+                showImgSource = imgSource[rand];
+                showImgLink = imgLink[rand];
+                showImgName = imgName[rand];
+                showImgAuthor = imgAuthor[rand];
+
+                if (imgSource.length !== 0) {
+                    $('body').css('background-image', showImgSource);
+                    $('#img-name').html(showImgName);
+                    $('#img-link').attr('href', showImgLink);
+                    $('#img-link').attr('title', showImgAuthor);
+                }
+            },
+            error: function(e) {
+
+            }
+        }).done(function() {
+            $('#loading-block').hide();
+        });
+    }
+
+    // initializeClock
+    function initializeClock() {
+        var timeinterval = setInterval(function() {
+            var now = new Date();
+            $('#clock-font').html(formatAMPM(now));
+        }, 1000);
+    }
+
+    // transfer AM/PM
+    function formatAMPM(date) {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+        return strTime;
+    }
+
+    // toogle search bar
+    $('#navbtn').click(function() {
+        $('#input-block').toggle();
+    });
+
+    // call function 
+    loadPhoto();
+    initializeClock();
+});
